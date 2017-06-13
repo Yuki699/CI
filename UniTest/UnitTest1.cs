82139e0fc461c4be5dd09622e558cd28feb3db77
yuki <lukasj8@gmail.com>
2017-06-13 11:50:28 +0200

    gf

---
+++ UniTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Min()
+        {
+            var res = Program.Minus(4, 2);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
